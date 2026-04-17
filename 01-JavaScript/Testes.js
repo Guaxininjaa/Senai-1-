64a5cd67f7fbb9112c71a807e37d3a4efd86ea6a
Guaxininjaa <feh_silvas@hotmail.com>
2026-04-16 22:14:46 -0300

    Primeira aula JavaScript

---
+++ 01-JavaScript/Testes.js
@@ -0,0 +1,20 @@
+// // alert("Pagina indisponivel! Atualize e tente novamente.")
+// // alert("Segunda mensagem")
+
+// let nome 
+
+// nome = "Felipe"
+// // alert("Nome: " + nome)
+// // let idade = 29
+// // // alert(" idade: " + idade)
+// // // alert("Nome: " + nome + "\nidade: " + idade)
+// // nome = prompt("Digite seu nome:")
+// // alert("Bem vindo: " + nome)
+
+
+let nota1
+nota1 = prompt("Digite a primeira nota: ")
+nota1 = Number(nota1)
+nota1 = nota1 + 1
+alert(nota1)
+
