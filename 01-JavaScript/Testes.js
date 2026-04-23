7493c3e0e7a4561912867a36ddd0f334c98e6611
Guaxininjaa <feh_silvas@hotmail.com>
2026-04-23 20:31:18 -0300

    add exercício

--- 01-JavaScript/Testes.js
+++ 01-JavaScript/Testes.js
@@ -11,10 +11,24 @@
 // // nome = prompt("Digite seu nome:")
 // // alert("Bem vindo: " + nome)
 
+// =================================================
+// let nota1
+// nota1 = prompt("Digite a primeira nota: ")
+// nota1 = Number(nota1)
+// nota1 = nota1 + 1
+// alert(nota1)
 
-let nota1
-nota1 = prompt("Digite a primeira nota: ")
-nota1 = Number(nota1)
-nota1 = nota1 + 1
-alert(nota1)
+// =================================================
+
+let vitorias, empates
+let pontos
+
+vitorias = prompt("Vitorias: ")
+vitorias = Number(vitorias)
+
+empates = Number(prompt("Empates"))
+
+pontos = vitorias*3 + empates
+
+alert("Total de Pontos: " + pontos)
 
