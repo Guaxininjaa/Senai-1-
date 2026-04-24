369b5c937ae538e3d2beda7c31600fabd4639a3c
Guaxininjaa <feh_silvas@hotmail.com>
2026-04-23 21:50:11 -0300

    add pe pequeno

---
+++ 01-JavaScript/pePequeno.js
@@ -0,0 +1,16 @@
+// alert("Alo")
+
+
+// infos
+ let preco, quantidade, valorTotal
+
+ //  leituras
+alert("Oi")
+ quantidade = Number(prompt("Quantidade:"))
+preco = Number(prompt("Preço R$: "))
+// Proc
+valorTotal = quantidade * preco
+// saidas
+console.log("Valor a receber: R$" + valorTotal.toFixed(2))
+ 
+
