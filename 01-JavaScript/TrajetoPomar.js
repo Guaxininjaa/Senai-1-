74a94d9197a3b67f7afd0007d35df440076b9806
Guaxininjaa <feh_silvas@hotmail.com>
2026-04-27 19:28:43 -0300

    Exercicios

--- 01-JavaScript/TrajetoPomar.js
+++ 01-JavaScript/TrajetoPomar.js
@@ -1,3 +1,8 @@
-let qtdlaranja, qtdFinal
-let vendidas
+let iniial, final, Total
 
+inicial =Number (prompt ("Quantidade de laranja"))
+final = Number (prompt ("Quantidade final Laranja"))
+
+total = inicial - final
+
+console.log ("total de laranja vendidas: " + total)
